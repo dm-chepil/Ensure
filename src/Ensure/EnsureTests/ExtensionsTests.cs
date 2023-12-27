666320d5aca79d9ee8c5d44aaae4380be178b1b1
dmytro.chepil <d.chepil.ext@sportradar.com>
2023-12-26 22:23:03 -0800

    Added unit tests

--- src/Ensure/EnsureTests/ExtensionsTests.cs
+++ src/Ensure/EnsureTests/ExtensionsTests.cs
@@ -1,3 +1,6 @@
+using EnsureCore;
+using EnsureCore.Extensions;
+
 namespace EnsureTests
 {
     public class ExtensionsTests
@@ -5,7 +8,27 @@ namespace EnsureTests
         [Fact]
         public void NotNull_InputIsNotNull_NoExceptionThrown()
         {
+            // Arrange
+            var input = new object();
 
+            // Act
+            Ensure.Is.NotNull(input, "", "test message");
+
+            // Assert
+        }
+
+        [Fact]
+        public void NotNull_InputIsNull_ThrowsAnException()
+        {
+            // Arrange
+            object input = null;
+
+            // Act
+
+            var action = () => Ensure.Is.NotNull(input, "", "test message");
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(action);
         }
     }
 }
